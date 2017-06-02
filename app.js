b6870e29467b8db040aff04a835555797ca85dea
Tonny Pham <jwall149@gmail.com>
2017-06-02 17:26:22 +0900

    Fix bug on missing jquery package

--- app.js
+++ app.js
@@ -115,35 +115,10 @@ app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }))
  * Primary app routes.
  */
 app.get('/', homeController.index);
-// app.get('/login', userController.getLogin);
-// app.post('/login', userController.postLogin);
-// app.get('/logout', userController.logout);
-// app.get('/forgot', userController.getForgot);
-// app.post('/forgot', userController.postForgot);
-// app.get('/reset/:token', userController.getReset);
-// app.post('/reset/:token', userController.postReset);
-// app.get('/api/signup', userController.getSignup);
 app.post('/api/login', userController.postLogin);
 app.post('/api/logout', passportConfig.isAuthenticated, userController.postLogout);
 app.post('/api/signup', userController.postSignup);
 app.post('/api/checkEmail', userController.postCheckEmail);
-// app.get('/account', passportConfig.isAuthenticated, userController.getAccount);
-// app.post('/account/profile', passportConfig.isAuthenticated, userController.postUpdateProfile);
-// app.post('/account/password', passportConfig.isAuthenticated, userController.postUpdatePassword);
-// app.post('/account/delete', passportConfig.isAuthenticated, userController.postDeleteAccount);
-// app.get('/account/unlink/:provider', passportConfig.isAuthenticated, userController.getOauthUnlink);
-
-/**
- * OAuth authentication routes. (Sign in)
- */
-// app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'public_profile'] }));
-// app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), (req, res) => {
-//   res.redirect(req.session.returnTo || '/');
-// });
-// app.get('/auth/google', passport.authenticate('google', { scope: 'profile email' }));
-// app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
-//   res.redirect(req.session.returnTo || '/');
-// });
 
 app.use((err, req, res, next) => {
   console.log(err);
