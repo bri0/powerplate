bc172ad325b8e38fa79aa5dd13ce55a41ed25d7d
Huy Dinh <huystalker@gmail.com>
2017-06-12 15:57:03 +0900

    Use material components for web

--- client/injectGlobal.js
+++ client/injectGlobal.js
@@ -3,6 +3,7 @@ window.jQuery = require('jquery/dist/jquery.slim.min');
 window._ = require('underscore/underscore-min');
 window.swal = require('sweetalert2/dist/sweetalert2.min');
 require('bootstrap/dist/js/bootstrap.min');
+window.mdc = require('material-components-web/dist/material-components-web.min');
 
 global.spinner = {
   show() {
